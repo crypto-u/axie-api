e832fff91fe010e1093093c906c0f7c32a8cfe10
Chisomo Wisck <chisomowisck@gmail.com>
2021-12-22 01:29:20 +0200

    hello

--- server.js
+++ server.js
@@ -4,40 +4,25 @@ const cors = require("cors");
 require('dotenv').config();
 const axios = require('axios')
 const Sequelize = require("sequelize");
-
 const app = express();
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 
-
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // database
 const db = require("./app/models");
 const Player = db.player;
 
-// db.sequelize.sync();
-// force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
-
-
-// simple route
+// routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to dicord bot application." });
 });
-
-// routes
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 require('./app/routes/player.routes')(app);
@@ -47,7 +32,17 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('message', async msg => {
-  switch (msg.content) {
+
+  let msgConent = msg.content;
+  let discordUser = "";
+
+  if(msgConent.substring(0, 8).toLowerCase() == "!invites"){
+    msgConent = msgConent.replace(/\s/g, "")
+    discordUser = msgConent.substring(8).toLowerCase();
+    msgConent = msgConent.substring(0, 8).toLowerCase();
+  }
+
+  switch (msgConent) {
     case "!test":
       msg.reply("Test 123!");
       break;
@@ -55,48 +50,49 @@ client.on('message', async msg => {
       case "!invites":
         msg.guild.invites.fetch().then((invites) => {
           const inviteCounter = {
-            bob: 19,
-            joe: 30,
+           
           }
       
           invites.forEach((invite) => {
             const { uses, inviter } = invite
             const { username, discriminator } = inviter
       
-            const name = `${username}#${discriminator}`
+            const name = `${username.toLowerCase().replace(/\s/g, "")}#${discriminator}`
       
             inviteCounter[name] = (inviteCounter[name] || 0) + uses
           })
+
+          
       
-          let replyText = 'Invites:'
+          let replyText = ''
       
           const sortedInvites = Object.keys(inviteCounter).sort(
             (a, b) => inviteCounter[b] - inviteCounter[a]
           )
-      
+        
+          //sortedInvites.length = 5
+
           //console.log(sortedInvites)
-      
-          sortedInvites.length = 4
-      
-          for (const invite of sortedInvites) {
+ 
+          const result = sortedInvites.filter(word => word == discordUser);
+
+          if(result.length == 0 ){
+            replyText += "Either user does not exists or you have typed a wrong discord name & tag"+ " \n" +
+             "Command: !invites yourname#yourTag E.g great#1024";
+
+          }
+
+//console.log(result);
+    
+          for (const invite of result) {
             const count = inviteCounter[invite]
             replyText += `\n${invite} has invited ${count} member(s)!`
           }
       
           msg.channel.send(replyText)
         })
-        // msg.guild.invites.fetch()
-        // .then(guild => {
-        //   console.log(guild)
-        // })
-        // .catch(console.error)
+ 
         break;
-    //our meme command below
-    case "!meme":
-      msg.channel.send("Here's your meme!"); //Replies to user command
-      const img = await getMeme(); //fetches an URL from the API
-      msg.channel.send(img); //send the image URL
-      break;
     
       case "!user":
 
@@ -146,32 +142,12 @@ async function getMeme(){
 async function getSLP(){
 
   const res = await axios.get('https://game-api.axie.technology/api/v1/ronin:fd809c29d00e34696b03c5bf6befad589aede36d');
-  //console.log(res.data.total_slp)
   return res.data;
 }
 
-//make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); 
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-// function initial() {
-//   Role.create({
-//     id: 1,
-//     name: "user"
-//   });
- 
-//   Role.create({
-//     id: 2,
-//     name: "moderator"
-//   });
- 
-//   Role.create({
-//     id: 3,
-//     name: "admin"
-//   });
-// }
